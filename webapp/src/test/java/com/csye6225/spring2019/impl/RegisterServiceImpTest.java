4511291877bb7b7bd2e3b9e0520f03753cdb5214
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-01-28 17:24:47 -0500

    update checkmethod

---
+++ webapp/src/test/java/com/csye6225/spring2019/impl/RegisterServiceImpTest.java
@@ -0,0 +1,55 @@
+package com.csye6225.spring2019.impl;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* RegisterServiceImp Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jan 28, 2019</pre> 
+* @version 1.0 
+*/ 
+public class RegisterServiceImpTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getTime() 
+* 
+*/ 
+@Test
+public void testGetTime() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: registerAccount(Account account) 
+* 
+*/ 
+@Test
+public void testRegisterAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: checkAccount(Account account) 
+* 
+*/ 
+@Test
+public void testCheckAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
