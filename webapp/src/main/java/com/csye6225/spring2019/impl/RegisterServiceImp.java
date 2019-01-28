a931b5f3c227577f3e56e3358135d9a9baebf84a
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-01-27 22:00:07 -0500

    update

--- webapp/src/main/java/com/csye6225/spring2019/impl/RegisterServiceImp.java
+++ webapp/src/main/java/com/csye6225/spring2019/impl/RegisterServiceImp.java
@@ -1,23 +1,53 @@
 package com.csye6225.spring2019.impl;
 
 import com.csye6225.spring2019.entity.Account;
+import com.csye6225.spring2019.repository.UserRepository;
 import com.csye6225.spring2019.service.RegisterService;
+import io.micrometer.core.instrument.util.StringUtils;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Log4j2
 @Service
 public class RegisterServiceImp implements RegisterService {
 
+    @Autowired
+
+    private UserRepository userRepository;
+    @Override
     public String getTime() {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-DD HH:mm:ss");
         LocalDateTime now  = LocalDateTime.now();
         return dateTimeFormatter.format(now);
     }
 
-    public void registerAccount(Account account){
+    @Override
+    public boolean registerAccount(Account account){
         //TODO
+        if(account == null || StringUtils.isEmpty(account.getEmailAddress()) || StringUtils.isEmpty(account.getPassword()))
+            return false;
+        try {
+            Account a = userRepository.findByEmailAddress(account.getEmailAddress());
+            if(a!= null)
+                return false;
+            String p= account.getPassword();
+            //p
+            //add into Database
+        }catch (Exception e){
+            log.info("UnExpected ERROR");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean checkAccount(Account account) {
+
+        return false;
     }
 
 }
