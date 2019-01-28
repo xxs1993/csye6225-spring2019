3d49c828645ddc6a203192796c917523c540f095
Zehua <ma.zeh@husky.neu.edu>
2019-01-27 18:01:49 -0800

    update Account.java

--- webapp/src/main/java/com/csye6225/spring2019/Entity/Account.java
+++ webapp/src/main/java/com/csye6225/spring2019/Entity/Account.java
@@ -1,7 +1,14 @@
 package com.csye6225.spring2019.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 @Entity
 @Table(name = "user")
 public class Account {
@@ -10,35 +17,35 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name = "emailAddress")
-    private String emailAddress;
+    @Column(name = "EMAIL_ADDRESS")
+    private String EMAIL_ADDRESS;
 
-    @Column(name = "password")
-    private String password;
+    @Column(name = "PWD_STRING")
+    private String PWD_STRING;
 
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmailAddress() {
+//        return emailAddress;
+//    }
+//
+//    public void setEmailAddress(String emailAddress) {
+//        this.emailAddress = emailAddress;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
