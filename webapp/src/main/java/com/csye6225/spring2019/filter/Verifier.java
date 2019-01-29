91674a042f0d9d79c3fb76014efe2028b1b87aa9
xxs1993 <xiao.x@husky.neu.edu>
2019-01-29 16:20:43 -0500

    update

--- webapp/src/main/java/com/csye6225/spring2019/filter/Verifier.java
+++ webapp/src/main/java/com/csye6225/spring2019/filter/Verifier.java
@@ -4,23 +4,27 @@ import com.csye6225.spring2019.entity.Account;
 import com.google.common.base.Splitter;
 import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.log4j.Log4j2;
-import org.apache.tomcat.util.codec.binary.Base64;
-import sun.misc.BASE64Decoder;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 @Log4j2
 public class Verifier {
-    public static Account isVerified(String auth) throws IOException {
+    public static Account isVerified(String auth)  {
         if(StringUtils.isEmpty(auth)) return null;
         String basic = "Basic";
         if(!auth.contains(basic)) return null;
-
-        BASE64Decoder decoder = new BASE64Decoder();
-        String accpString = new String(decoder.decodeBuffer(auth.substring(basic.length()).trim()));
-        List<String> list = Splitter.on(":").trimResults().splitToList(accpString);
+        List<String> list ;
+        String accpString = auth.substring(basic.length()).trim();
+        try {
+            accpString = new String(Base64.getDecoder().decode(accpString));
+            list = Splitter.on(":").trimResults().splitToList(accpString);
+        }catch (Exception e){
+            log.info(String.format("Invalid input:%s",accpString),e);
+            return null;
+        }
         if(list.size() !=2){
             log.warn(String.format(Arrays.toString(list.toArray())));
             return null;
