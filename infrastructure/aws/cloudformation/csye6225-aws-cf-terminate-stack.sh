1ffbeac836f34a6feb9c859d633011a761b573ae
Alex <jin.yim@husky.neu.edu>
2019-02-05 13:24:44 -0500

    some changes

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+echo "Input the stack name which you want to terminate"
+read name
+aws cloudformation delete-stack --stack-name $name
+aws cloudformation wait stack-delete-complete --stack-name $name
+aws cloudformation describe-stacks
+echo "stack delete successfully"
