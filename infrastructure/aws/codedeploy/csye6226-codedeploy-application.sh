2b56dd6f5470957db2c737e8cf1c70f4f54e5e82
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-03-08 19:00:39 -0500

    Update csye6226-codedeploy-application.sh

--- infrastructure/aws/codedeploy/csye6226-codedeploy-application.sh
+++ infrastructure/aws/codedeploy/csye6226-codedeploy-application.sh
@@ -10,6 +10,6 @@ fi
 if [ -z "$value" ]; then
 	value="csye6225_value"
 fi
-re=$(cat ec2-tag.json|sed "s/1/$key/g"|sed "s/csye6225_value/$value/g" )
+re=$(cat ec2-tag.json|sed "s/csye6225_key/$key/g"|sed "s/csye6225_value/$value/g" )
 role_arn=$(aws iam get-role --role-name CodeDeployServiceRole --query 'Role.Arn'|sed 's/\"//g')
 aws deploy create-deployment-group --application-name csye6225-webapp --deployment-group-name csye6225-webapp-deployment --service-role-arn $role_arn --deployment-style deploymentType=IN_PLACE,deploymentOption=WITHOUT_TRAFFIC_CONTROL --deployment-config-name CodeDeployDefault.AllAtOnce --auto-rollback-configuration enabled=true,events=DEPLOYMENT_FAILURE --ec2-tag-set "$re"
