392dbde8cdad9ff3b77ce8ee8359bbdea4f8145f
xxs1993 <xiao.x@husky.neu.edu>
2019-03-11 14:46:26 -0400

    update

---
+++ infrastructure/aws/cloudformation/csye-6225-aws-cf-create-role-stack.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -e
+echo "Input  stack name"
+read name
+echo "Input bucket name"
+read bucket
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-role.yaml --capabilities "CAPABILITY_NAMED_IAM" --parameters  "ParameterKey=bucket,ParameterValue=$bucket"
+aws cloudformation wait stack-create-complete --stack-name $name
+aws cloudformation describe-stacks
+echo "stack create successfully"
