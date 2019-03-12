eb33494241d7df3701a2327fd751490fe4957037
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-03-12 13:16:45 -0400

    update

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 set -e
 amiId=`aws ec2 describe-images --owners self --filters "Name=root-device-type,Values=ebs" | grep -o '"ImageId": *"[^"]*"' | grep -o '"[^"]*"$' | sed 's/\"//g' | head -n 1`
+RDS=`aws rds --region us-east-1 describe-db-instances --query "DBInstances[*].Endpoint.Address"`
 echo "Input application stack name"
 read name
 echo "Input reference stack name"
 read refStackName
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-application.yaml  --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$amiId"
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-application.yaml  --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$amiId" "ParameterKey=RDSEndpoint,ParameterValue=$RDS"
 echo "Processing, please wait"
 aws cloudformation wait stack-create-complete --stack-name $name
 aws cloudformation describe-stacks
