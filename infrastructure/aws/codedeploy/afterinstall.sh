d46b669def4de833b9d966e821392ecb25a3f8ee
Alex <jin.yim@husky.neu.edu>
2019-03-13 22:29:01 -0400

    change

--- infrastructure/aws/codedeploy/afterinstall.sh
+++ infrastructure/aws/codedeploy/afterinstall.sh
@@ -3,7 +3,7 @@
 # update the permission and ownership of WAR file in the tomcat webapps directory
 #sudo service tomcat8 stop
 
-cd /var/lib/tomcat8/webapps
-sudo chown tomcat8:tomcat8 ROOT.war
+cd /var/lib/tomcat7/webapps
+sudo chown tomcat7:tomcat7 csye6225-0.0.1-SNAPSHOT.war
 sudo chmod 777 ROOT.war
 sudo systemctl restart tomcat8
