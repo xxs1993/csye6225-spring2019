a49423f67726726652c05ef71be8a082321327aa
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-03-14 21:54:55 -0400

    Update afterinstall.sh

--- infrastructure/aws/codedeploy/afterinstall.sh
+++ infrastructure/aws/codedeploy/afterinstall.sh
@@ -5,7 +5,7 @@
 #sudo service tomcat8 stop
 cd /
 cd usr/share/tomcat/webapps
-sudo chown tomcat:tomcat csye6225-0.0.1-SNAPSHOT.war
-sudo chmod 777 csye6225-0.0.1-SNAPSHOT.war
+sudo chown tomcat:tomcat ROOT.war
+sudo chmod 777 ROOT.war
 sudo systemctl restart tomcat
 
