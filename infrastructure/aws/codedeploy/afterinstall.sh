802e1be7de95ebbabc84202b0699cc799ee47254
Zehua <ma.zeh@husky.neu.edu>
2019-03-15 20:33:59 -0400

    update tangmumao path

--- infrastructure/aws/codedeploy/afterinstall.sh
+++ infrastructure/aws/codedeploy/afterinstall.sh
@@ -3,7 +3,7 @@
 # update the permission and ownership of WAR file in the tomcat webapps directory
 #sudo service tomcat8 stop
 cd /
-cd usr/share/tomcat/webapps
+cd /opt/tomcat/latest/webapps
 sudo chown tomcat:tomcat csye6225-0.0.1-SNAPSHOT.war
 sudo chmod 777 csye6225-0.0.1-SNAPSHOT.war
 sudo systemctl restart tomcat
