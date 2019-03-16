a83153ccf2acbf93b70c1914bdabaa488a41df70
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-03-16 12:49:04 -0400

    webapp tomcat

---
+++ webapp/src/main/java/com/csye6225/spring2019/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.csye6225.spring2019;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServletInitializer.class);
+	}
+
+}
+
