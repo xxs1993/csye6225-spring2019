c1c87eec5e7f7a60205947840d1152cc5641d986
xxs1993 <xiao.x@husky.neu.edu>
2019-03-15 20:37:34 -0400

    update

--- webapp/src/main/java/com/csye6225/spring2019/entity/Note.java
+++ webapp/src/main/java/com/csye6225/spring2019/entity/Note.java
@@ -2,10 +2,12 @@ package com.csye6225.spring2019.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -13,13 +15,26 @@ import java.util.List;
 @Setter
 @ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Note {
+@Table(name="note")
+@Entity
+public class Note  {
+    @Id
+    @Column(name = "id",columnDefinition = "varchar(36) NOT NULL")
     private String id;
+
+    @Column(name = "user_id",columnDefinition = "int(11) DEFAULT '0'")
     private int userId;
+
+    @Column(name = "title",columnDefinition = "varchar(255) DEFAULT ''")
     private String title;
+    @Column(name = "content",columnDefinition = "varchar(255) DEFAULT ''")
     private String content;
+    @Column(name = "create_time",columnDefinition = "timestamp NULL DEFAULT CURRENT_TIMESTAMP")
     private Timestamp createTime;
+    @Column(name = "update_time",columnDefinition = "timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private Timestamp updateTime;
+
+    @Transient
     private List<Attachment> attachments;
 
 }
