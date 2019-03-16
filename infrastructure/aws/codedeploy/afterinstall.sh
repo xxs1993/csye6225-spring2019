b88b2530a556c0a88ea10fe994f5f1e5be8ef39c
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-03-15 22:04:07 -0400

    Update afterinstall.sh

--- infrastructure/aws/codedeploy/afterinstall.sh
+++ infrastructure/aws/codedeploy/afterinstall.sh
@@ -4,7 +4,6 @@
 # update the permission and ownership of WAR file in the tomcat webapps directory
 #sudo service tomcat8 stop
 cd /
-
 cd opt/tomcat/latest/webapps
 sudo chown tomcat:tomcat ROOT.war
 sudo chmod 777 ROOT.war
