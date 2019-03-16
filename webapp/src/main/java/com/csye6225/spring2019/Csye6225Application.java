ff04d43b5ff66bbb2837577d1fb533c84efb59d5
xxs1993 <xiao.x@husky.neu.edu>
2019-03-16 14:32:33 -0400

    update

--- webapp/src/main/java/com/csye6225/spring2019/Csye6225Application.java
+++ webapp/src/main/java/com/csye6225/spring2019/Csye6225Application.java
@@ -1,15 +1,24 @@
 package com.csye6225.spring2019;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//@EnableAutoConfiguration
 @SpringBootApplication
-public class Csye6225Application{
+public class Csye6225Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		System.out.println("13333");SpringApplication.run(Csye6225Application.class, args);
+		SpringApplication.run(Csye6225Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Csye6225Application.class);
 	}
 
 }
