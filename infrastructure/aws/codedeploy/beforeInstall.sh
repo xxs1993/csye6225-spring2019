1d1f840bd7ac24f8897517426dd7323585bfe266
xxs1993 <xiao.x@husky.neu.edu>
2019-03-21 15:02:21 -0400

    update

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -2,3 +2,4 @@
 # change to tomcat webapps directory.
 # this directory will be different for different tomcat versions.
 sudo rm -rf /opt/tomcat/latest/webapps/*
+sudo rm -rf /opt/tomcat/latest/logs/csye6225.log
