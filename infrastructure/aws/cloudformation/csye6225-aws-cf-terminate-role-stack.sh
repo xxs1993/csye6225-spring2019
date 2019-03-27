aa9ff9a753768d8bab6729b7c984444019cd84d4
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-03-27 11:56:25 -0400

    update

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-role-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-role-stack.sh
@@ -3,7 +3,8 @@ set -e
 echo "Input the stack name which you want to terminate"
 read name
 aws cloudformation delete-stack --stack-name $name
-aws iam detach-user-policy --user-name circleci --policy-arn arn:aws:iam::aws:policy/AWSLambdaFullAccessecho "Processing, please wait"
+aws iam detach-user-policy --user-name circleci --policy-arn arn:aws:iam::aws:policy/AWSLambdaFullAccess
+echo "Processing, please wait"
 aws cloudformation wait stack-delete-complete --stack-name $name
 aws cloudformation describe-stacks
 echo "stack delete successfully"
