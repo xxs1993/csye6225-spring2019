9b56828bc501d327981ffd964d7ec63496003dd1
Zehua <ma.zeh@husky.neu.edu>
2019-04-01 16:16:39 -0400

    updated SSL

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -11,7 +11,8 @@ read roleStackName
 echo "Input your husky-id on S3 bucket for WebApp"
 read S3BucketName
 lambdaRoleArn=$(aws iam get-role --role-name "LambdaRole" --query "Role.Arn" --output 'text')
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-auto-scaling-application.yaml  --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$amiId" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=rdsDBUser,ParameterValue=csye6225master" "ParameterKey=rdsDBPass,ParameterValue=csye6225password" "ParameterKey=S3BucketName,ParameterValue=csye6225-spring2019-$S3BucketName.me.csye6225.com" "ParameterKey=RDSEndPoint,ParameterValue=$RDS" "ParameterKey=roleArn,ParameterValue=$lambdaRoleArn" "ParameterKey=fromEmail,ParameterValue=noreply@noreply.csye6225-spring2019-${S3BucketName}.me" "ParameterKey=huskyId,ParameterValue=${S3BucketName}"
+certificateArn=$(aws acm list-certificates --query 'CertificateSummaryList[0].CertificateArn' --output text)
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-auto-scaling-application.yaml  --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$amiId" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=rdsDBUser,ParameterValue=csye6225master" "ParameterKey=rdsDBPass,ParameterValue=csye6225password" "ParameterKey=S3BucketName,ParameterValue=csye6225-spring2019-$S3BucketName.me.csye6225.com" "ParameterKey=RDSEndPoint,ParameterValue=$RDS" "ParameterKey=roleArn,ParameterValue=$lambdaRoleArn" "ParameterKey=fromEmail,ParameterValue=noreply@noreply.csye6225-spring2019-${S3BucketName}.me" "ParameterKey=huskyId,ParameterValue=${S3BucketName}" "ParameterKey=certificateArn,ParameterValue=$certificateArn"
 echo "Processing, please wait"
 aws cloudformation wait stack-create-complete --stack-name $name
 aws cloudformation describe-stacks
