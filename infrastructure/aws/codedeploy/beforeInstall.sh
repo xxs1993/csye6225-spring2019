fc7752a63819d65ffdab6cdf23da95a03f808634
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-04-05 18:33:28 -0400

    update before install

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # change to tomcat webapps directory.
 # this directory will be different for different tomcat versions.
-sudo rm -rf /opt/tomcat/latest/webapps/*
+sudo rm -rf /opt/tomcat/latest/webapps/docs  /opt/tomcat/latest/webapps/examples /opt/tomcat/latest/webapps/host-manager  /opt/tomcat/latest/webapps/manager /opt/tomcat/latest/webapps/ROOT
