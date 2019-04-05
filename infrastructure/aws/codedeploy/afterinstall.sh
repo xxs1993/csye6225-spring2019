b87f2a3e5504d9487d9fcceab7b042f6c29b94f5
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-04-05 17:59:01 -0400

    Update afterinstall.sh

--- infrastructure/aws/codedeploy/afterinstall.sh
+++ infrastructure/aws/codedeploy/afterinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # update the permission and ownership of WAR file in the tomcat webapps directory
 #sudo service tomcat8 stop
-sudo su
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
@@ -9,8 +8,5 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -s
 ## may change the service name
 sudo systemctl start amazon-cloudwatch-agent
-cd /opt/tomcat/latest/webapps
-sudo chown tomcat:tomcat ROOT.war
-sudo chmod 755 ROOT.war
-sudo systemctl restart tomcat
+sudo systemctl start tomcat
 
