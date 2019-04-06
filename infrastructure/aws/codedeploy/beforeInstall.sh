e70fe957d4af52c4ffeef5d5d97e5d3ac620b692
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-04-05 21:31:52 -0400

    Update beforeInstall.sh

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -7,4 +7,5 @@ do
   sleep 2
 done
 echo "find flag"
+sudo systemctl stop tomcat
 sudo rm -rf /opt/tomcat/latest/webapps/docs  /opt/tomcat/latest/webapps/examples /opt/tomcat/latest/webapps/host-manager  /opt/tomcat/latest/webapps/manager /opt/tomcat/latest/webapps/ROOT
