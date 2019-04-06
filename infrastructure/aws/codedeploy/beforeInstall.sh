566e87e0ca5c0d0e686baa80b7598e9c592f1cc9
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-04-05 21:00:56 -0400

    Update beforeInstall.sh

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -3,6 +3,8 @@
 # this directory will be different for different tomcat versions.
 while [ ! -f /home/centos/flag.txt ]
 do
+  echo "waiting for flag"
   sleep 2
 done
+echo "find flag"
 sudo rm -rf /opt/tomcat/latest/webapps/docs  /opt/tomcat/latest/webapps/examples /opt/tomcat/latest/webapps/host-manager  /opt/tomcat/latest/webapps/manager /opt/tomcat/latest/webapps/ROOT
