861fd12bbb3b22b43d1960bc9e6a03059cebb26a
Xuanshan Xiao <42723291+xxs1993@users.noreply.github.com>
2019-04-05 20:43:36 -0400

    Update beforeInstall.sh

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # change to tomcat webapps directory.
 # this directory will be different for different tomcat versions.
+while [ ! -f /home/centos/flag.txt ]
+do
+  sleep 2
+done
 sudo rm -rf /opt/tomcat/latest/webapps/docs  /opt/tomcat/latest/webapps/examples /opt/tomcat/latest/webapps/host-manager  /opt/tomcat/latest/webapps/manager /opt/tomcat/latest/webapps/ROOT
